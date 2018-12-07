7dcb8e1007056868e1c9326a33ca26191fcb6b69
sxxlearn2rock <sxx_uestc@163.com>
2018-12-07 14:55:35 +0800

    split in folders

---
+++ src/manage/app.vue
@@ -0,0 +1,12 @@
+<style lang="scss" scoped>
+h1{
+  font-size: 30px;
+  color: blue;
+}
+</style>
+
+<template>
+  <div class="wrapper">
+    <h1>Hollo manage!</h1>
+  </div>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -17,19 +17,26 @@ module.exports = function() {
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin(
     {
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: true
+      template: './src/user/index.html',
+      filename: 'user/index.html',
+      inject: true,
+      chunks:['user/index', 'vendors']
+    }),
+    new HtmlWebpackPlugin(
+    {
+      template: './src/manage/index.html',
+      filename: 'manage/index.html',
+      inject: true,
+      chunks:['manage/index', 'vendors']
     }),
     new MiniCssExtractPlugin({
-      filename: 'style_[hash].css'
+      filename: '[name].css?v=[contenthash]'
     }),
     new CopyWebpackPlugin([{
       from: path.resolve('src/assets'),
       to: path.resolve('dist/assets'),
       toType: 'dir'
-    }]),
-    new BundleAnalyzerPlugin()
+    }])
   ];
 
   // 将第三方库从bundle中抽离出来，单独打包进vendor.bundle
@@ -39,18 +46,23 @@ module.exports = function() {
       cacheGroups: {
         vendors: {
           test: /node_modules/,
-          minChunks: 1
+          minChunks: 1,
+          // 该chunk的名字，用于HtmlWebpackPlugin引入chunk
+          name: 'vendors'
         }
       }
     },
     // 将webpack的运行所需文件，从vendor中抽离出来
-    runtimeChunk: "single",
+    // runtimeChunk: "single",
     minimizer: []
   };
 
   // 根据不同的发布环境，进行不同的配置
   if (devMode) {
-
+    // 打包分析
+    plugins.push(new BundleAnalyzerPlugin({
+      analyzerPort: 9989
+    }));
   }else {
     plugins.push(new CleanWebpackPlugin(['dist']));
     optimization.minimizer.push(new UglifyJsPlugin({
@@ -63,12 +75,14 @@ module.exports = function() {
   return {
     mode: devMode ? 'development' : 'production',
     entry: {
-      app: './src/index.js',
+      'user/index': path.resolve(__dirname, 'src/user/index.js'),
+      'manage/index': path.resolve(__dirname, 'src/manage/index.js'),
       // vendor: Object.keys(packagejson.dependencies)//获取生产环境依赖的库
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: '[name].bundle_[hash].js'
+      filename: '[name].bundle_[hash:10].js',
+      chunkFilename: "[name].min.js?v=[hash:10]",
     },
     resolve: {
       // 指明第三方模块所在的文件夹
@@ -96,7 +110,6 @@ module.exports = function() {
           use: [
             devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
             'css-loader',
-            // 'postcss-loader'
             'sass-loader'
           ]
         },
